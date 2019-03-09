8e12486acbf8f0210ad71bfb0013065d4b2ac1d3
wunderabt <wunderabt@yahoo.co.in>
2019-03-10 00:10:33 +0100

    Update webdav_backup.sh

--- webdav_backup.sh
+++ webdav_backup.sh
@@ -6,16 +6,13 @@
 
 set -e
 set -x
-
 SOURCE_DIR=/home/john
 TARGET_DIR=/var/backup
-## remove old back-up dirs that are older than 10 days
-find ${TARGET_DIR} -depth -maxdepth 1 -ctime +10 -exec rm -fr {} \;
-
 ## create a new backup-up directory
 backup_dir=$(date +"%Y%m%d")
 mkdir -p ${TARGET_DIR}/${backup_dir}
-
+## remove old back-up dirs that are older than 10 days
+find ${TARGET_DIR} -depth -maxdepth 1 -ctime +10 -exec rm -fr {} \;
 ## run the back-up
 cd ${SOURCE_DIR}
 7z a -t7z -mhe=on -pyourpassword -xr\!\*@eaDir\* -xr\!\*Drive/pics\* -v200m ${TARGET_DIR}/${backup_dir}/bkup.7z .
