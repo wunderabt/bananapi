647f8a184cede1c0c7c6f6bdd5ff3fc076ed1b25
wunderabt <wunderabt@yahoo.co.in>
2021-12-18 16:24:56 +0100

    fix syntax error in boinc script

--- helper_scripts/boinc_nomorework.sh
+++ helper_scripts/boinc_nomorework.sh
@@ -1,1 +1,1 @@
-boinccmd --get_project_status | grep master URL | awk '{print()}' | xargs -I{} boinccmd --project {} nomorework
+boinccmd --get_project_status | grep "master URL" | awk '{print($3)}' | xargs -I{} boinccmd --project {} nomorework
