520501a747d4c42693a436a38c7540e7babc969a
wunderabt <wunderabt@yahoo.co.in>
2022-02-11 15:43:46 +0100

    include the set (=target) temperature

--- helper_scripts/fritz_thermostat_to_influx.py
+++ helper_scripts/fritz_thermostat_to_influx.py
@@ -19,9 +19,12 @@ def get_fritz_connection(credentials: Tuple[str, str, str]) -> FritzConnection:
     return FritzConnection(address=FRITZ_ADDRESS, password=credentials[2])
 
 
-def get_thermostat_readings(fritzconn: FritzConnection) -> List[Tuple[str, float]]:
+def get_thermostat_readings(fritzconn: FritzConnection) -> List[Tuple[str, float, float]]:
+    """
+    returns a list of 3-tuples with (device_name, current_temperature, set_temperature)
+    """
     fh = FritzHomeAutomation(fritzconn)
-    infos = [(x["NewDeviceName"], x["NewTemperatureCelsius"]/10) for x in fh.device_information()
+    infos = [(x["NewDeviceName"], float(x["NewTemperatureCelsius"]/10), float(x["NewHkrSetTemperature"]/10)) for x in fh.device_information()
              if x["NewProductName"] == "FRITZ!DECT 301" and x["NewTemperatureIsValid"] == "VALID"]
     return infos
 
@@ -33,7 +36,7 @@ def push_to_influx(thermostat_infos: List[Tuple[str, float]]) -> None:
         {
             "measurement": "temperature",
             "tags": {"device": x[0]},
-            "fields": {"C": x[1]},
+            "fields": {"C": x[1], "target_C": x[2]},
             "time": timestamp.isoformat()
         } for x in thermostat_infos
     ]
@@ -43,5 +46,6 @@ def push_to_influx(thermostat_infos: List[Tuple[str, float]]) -> None:
 if __name__ == "__main__":
     fc = get_fritz_connection(get_credentials(FRITZ_ADDRESS))
     ti = get_thermostat_readings(fc)
+    #print(ti)
     push_to_influx(ti)
 
